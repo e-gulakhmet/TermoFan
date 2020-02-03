bc912c8ef99276928ab2d3fab0ca04719d0160a0
WhoManTech <enes515254@gmail.com>
2020-02-03 12:51:37 -0500

    Update heater temp functions

--- src/heater.cpp
+++ src/heater.cpp
@@ -2,25 +2,99 @@
 
 
 
-Heater::Heater(uint8_t heater_pin, uint8_t temp_pin)
+Heater::Heater(uint8_t heater_pin, uint8_t sensor_pin)
     : heater_pin_(heater_pin)
+    , sensor_pin_(sensor_pin)
     , heater_temp_(0)
     , manual_temp_(0)
+    , max_temp_(120)
     , mode_(mOn)
+    , wireSensor(sensor_pin_)
+    , sensor(&wireSensor)
     {
-        pinMode(temp_pin, INPUT);
-        pinMode(heater_pin_, OUTPUT);
+        pinMode(heater_pin_, OUTPUT);      
     }
 
 
 
 void Heater::update() {
-    heater_temp_ = getTemp();
+    
+
+    switch (mode_) {
+        case mOn:
+            heater_temp_ = getTemp();
+
+            if (heater_temp_ > max_temp_) {
+                manual_temp_ = max_temp_ - 20;
+            }
+
+            break;
+
+        
+        case mOff:
+            manual_temp_ = 0;
+    }
+    
 }
 
 
 
 uint8_t Heater::getTemp(){
-    uint8_t temp = 40;
+    sensor.requestTemperatures();
+    uint8_t temp = sensor.getTempC(sensorAddress);
     return temp;
+}
+
+
+
+void Heater::setTemp(uint8_t manual_temp) {
+    if (manual_temp > max_temp_) {
+        manual_temp_ = max_temp_;
+    }
+    else if (manual_temp < 0) {
+        manual_temp_ = 0;
+    }
+    else {
+        manual_temp_ = manual_temp;
+    }
+}
+
+
+
+void Heater::addTemp() {
+    if (manual_temp_ >= max_temp_) {
+        manual_temp_ = max_temp_;
+    }
+    else {
+        manual_temp_ += 10;
+    }
+}
+
+
+
+void Heater::minTemp() {
+    if (manual_temp_ <= 0) {
+        manual_temp_ = 0;
+    }
+    else {
+        manual_temp_ -= 10;
+    }
+}
+
+
+
+void Heater::on() {
+    mode_ = mOn;
+}
+
+
+
+void Heater::off() {
+    mode_ = mOff;
+}
+
+
+
+void Heater::setMaxTemp(uint8_t max_temp) {
+    max_temp_ = max_temp;    
 }
--- src/heater.h
+++ src/heater.h
@@ -2,12 +2,14 @@
 #define _HEATER_H_
 
 #include <Arduino.h>
+#include <DallasTemperature.h>
+#include <OneWire.h>
 
 
 
 class Heater {
     public:
-        Heater(uint8_t heater_pin, uint8_t temp_pin);
+        Heater(uint8_t heater_pin, uint8_t sensor_pin);
 
         typedef enum {
             mOff,
@@ -18,15 +20,23 @@ class Heater {
         void setTemp(uint8_t manual_temp);
         void addTemp();
         void minTemp();
+        void on();
+        void off();
+        void setMaxTemp(uint8_t max_temp);
     
     private:
         uint8_t getTemp();
 
         uint8_t heater_pin_;
+        uint8_t sensor_pin_;
         uint8_t heater_temp_;
         uint8_t manual_temp_;
+        uint8_t max_temp_;
 
         Mode mode_;
+        OneWire wireSensor;
+        DallasTemperature sensor;
+        DeviceAddress sensorAddress;
 };
 
 
